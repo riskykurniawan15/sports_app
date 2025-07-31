c3fe8b0dd4bcbb5b265af170e78537adbe9e6805
Risky Kurniawan <>
2025-07-31 08:15:29 +0700

    add fallback response on web route

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::fallback(function () {
+    return response()->json([
+        'code' => [
+            'status' => 404,
+            'message' => 'The route ' . request()->path() . ' could not be found.'
+        ],
+        'data' => null
+    ], 404);
+});
